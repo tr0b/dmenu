9ada0083aaf6087dc92ef87d85cb90de49df409d
tr0b <trobinson3000600@gmail.com>
2023-12-24 17:08:56 -0600

    Will modify alpha transparency value to 90%

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xe6;     /* Amount of opacity. 0xff is opaque. 0xe6 90% opaque            */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
